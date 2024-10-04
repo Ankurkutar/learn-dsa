1b57a402fd37688637226c52ea3e440b7106448e
Ankur kutar <ankurkutar2000@gmail.com>
2024-10-04 20:47:08 +0530

    125 Valid Pallindrome

---
+++ leetcode/125_Valid_Palindrome.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+        bool isPalindrome(string s){
+            int left = 0; 
+            int right = s.size()-1;
+
+            while (left < right)
+            {
+                if(!isalnum(s[left])){
+                    left++;
+                }else if(!isalnum(s[right])){
+                    right--;
+                }else if(tolower(s[left]) != tolower(s[right])){
+                    return false;
+                }else{
+                    left++;
+                    right--;
+                }
+            }
+            return true;   
+        }
+};
+
+int main(){
+    Solution solution;
+    string testStr = "A man, a plan, a canal: Panama";  // Example palindrome
+
+    if(solution.isPalindrome(testStr)){
+        cout << "The string is a valid palindrome." << endl;
+    }else{
+        cout << "The string is not a valid palindrome." << endl;
+    }
+
+    return 0;
+}
