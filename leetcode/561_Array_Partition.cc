f282078b7289ee631ed8392b1351eb8614e78aa0
Ankur kutar <ankurkutar2000@gmail.com>
2024-11-04 20:39:28 +0530

    Array Partition using sorting method add element after one element

--- leetcode/561_Array_Partition.cc
+++ leetcode/561_Array_Partition.cc
@@ -7,7 +7,7 @@ int arrayPairSum(vector<int>& nums) {
     int sum = 0;
     sort(nums.begin(), nums.end());
     for(int i=0; i<nums.size(); i+=2){
-        sum += nums[i];
+        sum += nums[i]; // here aftr sorting adding elemnent bcz they also in pair in min ele
     }
     return sum;
 }
