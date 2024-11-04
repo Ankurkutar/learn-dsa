615e71aeeafb6aa62e4902b4dbbeef1342177f45
Ankur kutar <ankurkutar2000@gmail.com>
2024-11-04 20:37:51 +0530

    Array Partition

---
+++ leetcode/561_Array_Partition.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int arrayPairSum(vector<int>& nums) {
+    int sum = 0;
+    sort(nums.begin(), nums.end());
+    for(int i=0; i<nums.size(); i+=2){
+        sum += nums[i];
+    }
+    return sum;
+}
+
+
+int main() {
+//    vector<int> nums = {1, 4, 3, 2};
+   vector<int> nums = {6,2,6,5,1,2};
+
+    int result = arrayPairSum(nums);
+    cout << "The maximum sum of min elements in pairs is: " << result << std::endl;
+
+    return 0;
+}
