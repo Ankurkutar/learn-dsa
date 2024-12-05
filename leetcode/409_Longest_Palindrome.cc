ede991cbe78b99092167107458c41dd5435be80e
Ankur kutar <ankurkutar2000@gmail.com>
2024-12-05 19:46:49 +0530

    Longest Palindrome

---
+++ leetcode/409_Longest_Palindrome.cc
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+using namespace std;
+
+class Solution{
+    public:
+        int longestPalindrome(string s){
+            unordered_map<char, int>charCount;
+            for(char ch : s){
+                charCount[ch]++;
+            }
+
+            int length = 0;
+            bool oddFound = false;
+
+            for(auto &pair : charCount){
+                if(pair.second % 2 == 0){
+                    length += pair.second;
+                }else{
+                    length += pair.second-1;
+                    oddFound = true;
+                }
+            }
+
+            if(oddFound)
+                length++;
+            
+            return length;
+        }
+};
+
+
+int main() {
+    Solution solution;
+
+    // Test cases
+    string s1 = "abccccdd";
+    string s2 = "aabbccdde";
+
+    cout << "Longest Palindrome length for \"" << s1 << "\": " 
+         << solution.longestPalindrome(s1) << endl;
+
+    cout << "Longest Palindrome length for \"" << s2 << "\": " 
+         << solution.longestPalindrome(s2) << endl;
+
+    return 0;
+}
